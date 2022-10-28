fc99ced36398ed2c3c3e6aa2cac920e98d43baf4
JulianoKosloski <juliano.koslos@gmail.com>
2022-10-28 09:05:04 -0300

    connection db

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,5 +1,29 @@
-const db = require('mongoose')
 
 //see mongodb guidelines to set this up
 
-//...and write the rest
+const { MongoClient } = require("mongodb");
+const Db = process.env.ATLAS_URI;
+const client = new MongoClient(Db, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+ 
+var _db; //the names here are not that clear, gotta check naming conventions
+ 
+module.exports = {
+  connectToServer: function (callback) {
+    client.connect(function (err, db) {
+      // Verify we got a good "db" object
+      if (db)
+      {
+        _db = db.db("employees");
+        console.log("Successfully connected to MongoDB."); 
+      }
+      return callback(err);
+         });
+  },
+ 
+  getDb: function () {
+    return _db;
+  },
+};
